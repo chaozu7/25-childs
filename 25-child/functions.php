735b837757251b62173347322882052c74e52053
Kamila Nawrocka <kamabo.7@gmail.com>
2026-02-11 22:26:29 +0100

    fix: script load in footer

--- 25-child/functions.php
+++ 25-child/functions.php
@@ -7,4 +7,4 @@
 include(get_theme_file_path( 'includes/front/enqueue.php' ));
 
 //Hooks
-add_action( 'wp_enqueue_scripts', 'fz_enqueue');
+add_action( 'wp_enqueue_scripts', 'fz_enqueue', 20);
--- 25-child/includes/front/enqueue.php
+++ 25-child/includes/front/enqueue.php
@@ -2,10 +2,9 @@
 
 function fz_enqueue(){
 //register
-  
    wp_register_style('fz_styles', get_theme_file_uri('assets/css/style.css'), [], null); 
-   wp_register_script( 'fz_script', get_theme_file_uri('assets/js/scripts.js'), [], null, ['strategy' => 'defer', 'is_footer' => true]);
+   wp_register_script( 'fz_script', get_theme_file_uri('assets/js/scripts.js'), [], null, ['strategy' => 'defer', 'in_footer' => true]);
 //enqueue
-   wp_enqueue_style('fz_styles', 5);
+   wp_enqueue_style('fz_styles');
    wp_enqueue_script('fz_script');
 }
