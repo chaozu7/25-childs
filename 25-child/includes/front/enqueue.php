bbf9d3b2ce6efd5fe8339750b43b009224fa4552
Kamila Nawrocka <kamabo.7@gmail.com>
2026-02-11 22:14:49 +0100

    feat: register child theme

---
+++ 25-child/includes/front/enqueue.php
@@ -0,0 +1,11 @@
+<?php
+
+function fz_enqueue(){
+//register
+  
+   wp_register_style('fz_styles', get_theme_file_uri('assets/css/style.css'), [], null); 
+   wp_register_script( 'fz_script', get_theme_file_uri('assets/js/scripts.js'), [], null, ['strategy' => 'defer', 'is_footer' => true]);
+//enqueue
+   wp_enqueue_style('fz_styles', 5);
+   wp_enqueue_script('fz_script');
+}
